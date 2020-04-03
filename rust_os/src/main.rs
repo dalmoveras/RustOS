4e745274d83465bda89f0dc0310a5d5b8658d09e
Dalmo Veras Neto <verasnt@protonmail.com>
2020-04-03 19:33:56 -0300

    implemented panic without the help of stdlib

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+use core::panic::PanicInfo;
+//Func to be called on panic situations
+#[panic_handler]
+fn panic(_info: &PanicInfo)-> ! {
+    loop {}
 }
+
+fn main() {}
