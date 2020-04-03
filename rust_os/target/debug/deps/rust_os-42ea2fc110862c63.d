842867492c0c7fd0424131f25abf119d1eeb7d92
Dalmo Veras Neto <verasnt@protonmail.com>
2020-04-03 19:34:20 -0300

    cargo buildo stuff

---
+++ rust_os/target/debug/deps/rust_os-42ea2fc110862c63.d
@@ -0,0 +1,5 @@
+/home/perpetrator/Documents/projects/RustOS/rust_os/target/debug/deps/rust_os-42ea2fc110862c63: src/main.rs
+
+/home/perpetrator/Documents/projects/RustOS/rust_os/target/debug/deps/rust_os-42ea2fc110862c63.d: src/main.rs
+
+src/main.rs:
---
+++ rust_os/target/debug/deps/rust_os-cdf1740e969c6f10.d
@@ -0,0 +1,5 @@
+/home/perpetrator/Documents/projects/RustOS/rust_os/target/debug/deps/rust_os-cdf1740e969c6f10: src/main.rs
+
+/home/perpetrator/Documents/projects/RustOS/rust_os/target/debug/deps/rust_os-cdf1740e969c6f10.d: src/main.rs
+
+src/main.rs:
