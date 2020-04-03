369b6d3592c280053351ff972d032ec15721d5a8
Dalmo Veras Neto <verasnt@protonmail.com>
2020-04-03 19:51:48 -0300

    overwriting crt0

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,10 +1,15 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
 //Func to be called on panic situations
 #[panic_handler]
 fn panic(_info: &PanicInfo)-> ! {
     loop {}
 }
-
-fn main() {}
