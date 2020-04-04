ee119678132d3e0e4599ff8a3533500ac0dc93df
Dalmo Veras Neto <verasnt@protonmail.com>
2020-04-03 21:28:36 -0300

    added some comments

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,10 +1,11 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link the Rust std lib
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // do not mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    // the entry point
     loop {}
 }
 
