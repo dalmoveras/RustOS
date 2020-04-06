ee505de821de3b2a06550072f66f3d9185b8739d
Dalmo Veras Neto <verasnt@protonmail.com>
2020-04-06 15:25:19 -0300

    added repr(C) to laid out the struct fields

--- rust_os/src/vga_buffer.rs
+++ rust_os/src/vga_buffer.rs
@@ -20,3 +20,18 @@ pub enum Color {
     Yellow = 14,
     White = 15,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
